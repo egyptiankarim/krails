e4b78015339be610ecb08f4bc3fe531a5401deec
Karim Said <karim@egyptiankarim.com>
2015-01-19 14:14:09 -0500

    Forgot to drop some files in the new build script

--- krails.rb
+++ krails.rb
@@ -62,6 +62,8 @@ inside 'lib' do
       copy_file 'images.css'
       copy_file 'sticky-footer-navbar.css'
       copy_file 'typography.css.scss'
+      copy_file 'jumbotron.css'
+      copy_file 'dashboard.css'
     end
   end
 end
@@ -77,6 +79,10 @@ inside 'app' do
       copy_file 'application.css'
     end
   end
+  inside 'controllers' do
+    remove_file 'welcome_controller.rb'
+    copy_file 'welcome_controller.rb'
+  end
   inside 'helpers' do
     remove_file 'application_helper.rb'
     copy_file 'application_helper.rb'
@@ -85,12 +91,15 @@ inside 'app' do
     inside 'layouts' do
       remove_file 'application.html.erb'
       copy_file 'application.html.erb'
+      copy_file 'jumbotron.html.erb'
+      copy_file 'dashboard.html.erb'
     end
     inside 'shared' do
       copy_file '_foot.html.erb' 
       copy_file '_head.html.erb'  
       copy_file '_lorem.html.erb'  
       copy_file '_nav.html.erb'
+      copy_file '_short_lorem.html.erb'
     end
     inside 'welcome' do
       remove_file 'index.html.erb'
