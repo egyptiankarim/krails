16ca1baf696348f39bdf419df1bb58b95c02ad57
Karim Said <karim.a.said@nasa.gov>
2017-11-27 16:45:48 -0500

    Cleaned up layouts.

--- krails_root/app/helpers/application_helper.rb
+++ krails_root/app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
   def view_title(page_title)
     content_for(:title) { page_title }
   end
-  
+
   # Quick way to highlight active navigation bar areas.
-  def is_active(controller)
-    params[:controller] == controller ? "active" : nil
+  def is_active?(controller_action)
+    "#{params[:controller]}\##{params[:action]}" == controller_action ? "active" : nil
   end
 end
